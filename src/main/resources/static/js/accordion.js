0f4ace27736e9bb92473312836a6e408dc388c85
jjeongee <starstar117@naver.com>
2023-10-06 20:39:14 +0900

    community update by jjeongee

--- src/main/resources/static/js/accordion.js
+++ src/main/resources/static/js/accordion.js
@@ -1,24 +1,25 @@
-const accordionText = document.querySelector('.accordion_title_text');
-const accordionBtn = document.querySelector('.open_btn');
-const accordionContents = document.querySelector('.accordion_content');
-const informationCircle = document.querySelector('.info_circle');
-const informationBlack = document.querySelector('.info_circle_black');
-const bottomArrow = document.querySelector('.bottom_arrow');
-
-accordionBtn.addEventListener('click', () => {
-    if (accordionContents.classList.contains('dropdown')) {
-        accordionContents.classList.remove('dropdown');
-        accordionBtn.classList.remove('dropdown');
-        accordionText.classList.remove('dropdown');
-        informationCircle.classList.remove('dropdown');
-        informationBlack.classList.remove('dropdown');
-        bottomArrow.classList.remove('dropdown');
-    } else {
-        accordionContents.classList.add('dropdown');
-        accordionBtn.classList.add('dropdown');
-        accordionText.classList.add('dropdown');
-        informationCircle.classList.add('dropdown');
-        bottomArrow.classList.add('dropdown');
-        informationBlack.classList.add('dropdown');
+const accordionTitle = document.querySelectorAll('.accordion_title_text');
+let accordionBtn = document.querySelectorAll('.open_btn');
+const accordionContents = document.querySelectorAll('.accordion_content');
+const informationCircle = document.querySelectorAll('.info_circle');
+const informationBlack = document.querySelectorAll('.info_circle_black');
+const bottomArrow = document.querySelectorAll('.bottom_arrow');
+    for(let i=0;i<accordionBtn.length;i++){
+        accordionBtn[i].addEventListener('click', () => {
+            if (accordionContents[i].classList.contains('dropdown')) {
+                accordionContents[i].classList.remove('dropdown');
+                accordionBtn[i].classList.remove('dropdown');
+                accordionTitle[i].classList.remove('dropdown');
+                informationCircle[i].classList.remove('dropdown');
+                informationBlack[i].classList.remove('dropdown');
+                bottomArrow[i].classList.remove('dropdown');
+            } else {
+                accordionContents[i].classList.add('dropdown');
+                accordionBtn[i].classList.add('dropdown');
+                accordionTitle[i].classList.add('dropdown');
+                informationCircle[i].classList.add('dropdown');
+                bottomArrow[i].classList.add('dropdown');
+                informationBlack[i].classList.add('dropdown');
+            }
+        });
     }
-});
