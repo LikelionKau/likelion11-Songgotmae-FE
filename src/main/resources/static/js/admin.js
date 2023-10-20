3ab23da660d3daf3a96cdc61ae8dda6c5a74e4a9
KIMJINBBANG <0925taekwonjy@gmail.com>
2023-10-20 17:09:29 +0900

    10/20

---
+++ src/main/resources/static/js/admin.js
@@ -0,0 +1,57 @@
+function approve(){
+
+    const nickname = document.getElementById("nickname");
+    const kauemail = document.getElementById("kauemail");
+    const password = document.getElementById("password");
+
+    // const data = Response[5][1][1][2];
+
+    axios({
+        method:"POST",
+        // url: 'http://www.songgotmae.com:8080/api/v1/join',
+        url: 'http://ec2-13-209-226-19.ap-northeast-2.compute.amazonaws.com:8080/admin/v1/posts/{postId}/approve',
+        data:{
+            "nickname": nickname.value,
+            "kauEmail": kauemail.value,
+            "password": password.value,
+        }
+    })  .then(response => response.json())
+        .then((res)=>{
+            console.log(res);
+            alert('회원가입이 완료되었습니다.');
+            window.location.href = '../templates/login.html';
+        }).catch(error=>{
+        console.log(error);
+        alert(error.response.data.message);
+        // throw new Error(error);
+    });
+}
+
+function reject(){
+
+    const nickname = document.getElementById("nickname");
+    const kauemail = document.getElementById("kauemail");
+    const password = document.getElementById("password");
+
+    // const data = Response[5][1][1][2];
+
+    axios({
+        method:"POST",
+        // url: 'http://www.songgotmae.com:8080/api/v1/join',
+        url: 'http://ec2-13-209-226-19.ap-northeast-2.compute.amazonaws.com:8080/api/v1/join',
+        data:{
+            "nickname": nickname.value,
+            "kauEmail": kauemail.value,
+            "password": password.value,
+        }
+    })  .then(response => response.json())
+        .then((res)=>{
+            console.log(res);
+            alert('회원가입이 완료되었습니다.');
+            window.location.href = '../templates/login.html';
+        }).catch(error=>{
+        console.log(error);
+        alert(error.response.data.message);
+        // throw new Error(error);
+    });
+}
--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -1,8 +1,12 @@
+// let comiit = await Response.json();
+
 function signup(){
 
     const nickname = document.getElementById("nickname");
     const kauemail = document.getElementById("kauemail");
-    const password = document.getElementById("password")
+    const password = document.getElementById("password");
+
+    // const data = Response[5][1][1][2];
 
     axios({
         method:"POST",
@@ -13,12 +17,14 @@ function signup(){
             "kauEmail": kauemail.value,
             "password": password.value,
         }
-    }).then((res)=>{
+    })  .then(response => response.json())
+        .then((res)=>{
         console.log(res);
         alert('회원가입이 완료되었습니다.');
         window.location.href = '../templates/login.html';
     }).catch(error=>{
         console.log(error);
+        alert(error.response.data.message);
         // throw new Error(error);
     });
 }
