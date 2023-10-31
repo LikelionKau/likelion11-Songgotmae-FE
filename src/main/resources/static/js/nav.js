8fa7e6538027946b4ec238aeb217d9ef5944b3b4
KIMJINBBANG <0925taekwonjy@gmail.com>
2023-10-31 12:30:12 +0900

    after 10/30 total

--- src/main/resources/static/js/nav.js
+++ src/main/resources/static/js/nav.js
@@ -4,7 +4,7 @@ const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 // const navbarMenuDown = document.querySelector('.bg');
 const bodyDiv = document.querySelector('.main_div');
 
-navbarToggleBtn.addEventListener('click',()=>{
+navbarToggleBtn.addEventListener('click', () => {
     navbarMenu.classList.toggle('open');
     navbarToggleBtn.classList.toggle('open');
     // "open" 클래스가 부여되었을 때
@@ -14,15 +14,17 @@ navbarToggleBtn.addEventListener('click',()=>{
     }
 });
 
+
 document.addEventListener('click', (event) => {
     const targetElement = event.target;
     if (!targetElement.closest('.navbar_menu') && !targetElement.closest('.navbar__toggle-btn') || !targetElement.closest('.navbar__toggle-btn')) {
         navbarMenu.classList.remove('open');
         navbarToggleBtn.classList.remove('open');
         bodyDiv.classList.remove('open');
-        bodyDiv.style.marginTop='0';
+        bodyDiv.style.marginTop = '0';
     }
 });
+
 
 
 
