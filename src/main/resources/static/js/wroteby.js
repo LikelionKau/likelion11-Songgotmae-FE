f79f08dce71034884d0d59796d76c314f506aed1
seohyuna <128970914+seohyuna@users.noreply.github.com>
2023-11-06 19:41:48 +0900

    1106sunny

---
+++ src/main/resources/static/js/wroteby.js
@@ -0,0 +1,18 @@
+function approve(){
+
+    const memberId = document.getElementById("memberId")
+
+    axios({
+        method:"POST",
+        url: 'http://ec2-52-78-33-144.ap-northeast-2.compute.amazonaws.com:8080/api/v1/posts/wroteBy/{memberId}',
+        data: {
+            "memberId": memberId.value,
+        }
+    }).then(response => {
+        console.log(response.data);
+    })
+        .catch(error => {
+            console.error('API 요청 실패:', error);
+        });
+
+}
