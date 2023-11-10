af7f8e81eb255a3a2aa53873f5e7ecdf95eee1da
jjeongee <starstar117@naver.com>
2023-11-10 14:09:25 +0900

    navbarupdate

--- src/main/resources/static/js/nav.js
+++ src/main/resources/static/js/nav.js
@@ -3,14 +3,21 @@ const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
 const navbarBackground = document.querySelector('.navbar');
 const navbarMenuDown = document.querySelector('.bg');
 const bodyDiv = document.querySelector('.main_div');
+const communityBodyDiv = document.querySelector('.community_main_div');
+
 
 navbarToggleBtn.addEventListener('click', () => {
     navbarMenu.classList.toggle('open');
     navbarToggleBtn.classList.toggle('open');
-    // "open" 클래스가 부여되었을 때
-    if (bodyDiv.classList.toggle('open')) {
-        // 추가 여백을 설정
-        bodyDiv.style.marginTop = '100px'; // 예시로 100px로 설정
+    if(bodyDiv){
+        bodyDiv.classList.toggle('open');
+        bodyDiv.style.marginTop = '100px';
+    }
+    else if(communityBodyDiv){
+        communityBodyDiv.classList.toggle('open');
+        if(communityBodyDiv.classList.contains('open')){
+            communityBodyDiv.style.marginTop='120px';
+        }
     }
     else {
         console.log("Element not found.");
@@ -23,9 +30,18 @@ document.addEventListener('click', (event) => {
     if (!targetElement.closest('.navbar_menu') && !targetElement.closest('.navbar__toggle-btn') || !targetElement.closest('.navbar__toggle-btn')) {
         navbarMenu.classList.remove('open');
         navbarToggleBtn.classList.remove('open');
-        bodyDiv.classList.remove('open');
-        bodyDiv.style.marginTop = '0';
+        if(bodyDiv){
+            bodyDiv.classList.remove('open');
+            bodyDiv.style.marginTop = '0';
+        }
+        else if (!bodyDiv&& communityBodyDiv) {
+            // bodyDiv가 없고 communityBodyDiv만 있는 경우
+            communityBodyDiv.classList.remove('open');
+            communityBodyDiv.style.marginTop = '0';
+        }
     }
+
+
 });
 
 
