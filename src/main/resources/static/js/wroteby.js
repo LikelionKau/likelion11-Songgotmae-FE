929cfd6d23cb02aef0a4f0a1bff2cd903f644bc2
KIMJINBBANG <0925taekwonjy@gmail.com>
2023-11-16 17:09:12 +0900

    wow

--- src/main/resources/static/js/wroteby.js
+++ src/main/resources/static/js/wroteby.js
@@ -1,3 +1,25 @@
+// 로컬 스토리지에서 토큰을 읽어오기
+const token = localStorage.getItem('token');
+
+// 토큰을 로컬 스토리지에 저장
+localStorage.setItem('token', token);
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    function logout() {
+        localStorage.removeItem('token');
+        alert('로그아웃되었습니다.');
+    }
+
+    if (localStorage.getItem('token') !== 'null') {
+        // 토큰이 있으면 로그아웃 버튼으로 업데이트
+        loginButton.textContent = '로그아웃';
+        loginButton.addEventListener('click', logout);
+        nav_signup.textContent = '마이페이지';
+        nav_signup.href = '../templates/mypage.html';
+    }
+});
+
 function approve(){
 
     const memberId = document.getElementById("memberId")
